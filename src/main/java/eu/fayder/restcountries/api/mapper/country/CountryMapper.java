868fdeaff7f242f42256e38bae3ba498db62eb42
Julian Sasso <jsasso@itba.edu.ar>
2025-05-18 19:41:28 -0300

    Add CountryMapper interface

--- src/main/java/eu/fayder/restcountries/api/mapper/country/CountryMapper.java
+++ src/main/java/eu/fayder/restcountries/api/mapper/country/CountryMapper.java
@@ -5,8 +5,17 @@ import eu.fayder.restcountries.domain.countryinfo.country.Country;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
-@Mapper(componentModel = "spring")
+import java.net.URI;
+
+@Mapper(componentModel = "spring", uses = {LanguageMapper.class})
 public interface CountryMapper {
 
+    //@Mapping(target = "iso639_1", source = "iso6391")
+    //@Mapping(target = "iso639_2", source = "iso6392")
     CountryResponse toResponse(Country country);
+
+    default URI map(String value) {
+        return value != null ? URI.create(value) : null;
+    }
+
 }
