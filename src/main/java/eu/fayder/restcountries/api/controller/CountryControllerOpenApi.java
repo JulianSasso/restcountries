16928c8ed165e3de3d196c802f3d1a0e0be04b28
Julian Sasso <jsasso@itba.edu.ar>
2025-05-18 19:41:49 -0300

    Implement getAllCountries in CountryControllerOpenApi

--- src/main/java/eu/fayder/restcountries/api/controller/CountryControllerOpenApi.java
+++ src/main/java/eu/fayder/restcountries/api/controller/CountryControllerOpenApi.java
@@ -1,60 +1,74 @@
 package eu.fayder.restcountries.api.controller;
 
 import eu.fayder.restcountries.api.CountryApi;
-import eu.fayder.restcountries.domain.Country;
+import eu.fayder.restcountries.api.mapper.country.CountryMapper;
+import eu.fayder.restcountries.domain.CountryResponse;
+import eu.fayder.restcountries.domain.countryinfo.CountryInformationService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import javax.ws.rs.core.Response;
 import java.util.List;
 
+@RequestMapping("rest/v3")
+@RestController
+@RequiredArgsConstructor
 public class CountryControllerOpenApi implements CountryApi {
 
+    private final CountryInformationService countryService;
+    private final CountryMapper countryMapper;
+
     @Override
-    public ResponseEntity<List<Country>> getAllCountries(String fields) {
-        return CountryApi.super.getAllCountries(fields);
+    @GetMapping("all")
+    public ResponseEntity<List<CountryResponse>> getAllCountries(String fields) {
+        return ResponseEntity.ok(countryService.getAll().stream().map(countryMapper::toResponse).toList());
     }
 
     @Override
-    public ResponseEntity<List<Country>> getCountriesByAlphaCodes(String codes, String fields) {
+    public ResponseEntity<List<CountryResponse>> getCountriesByAlphaCodes(String codes, String fields) {
         return CountryApi.super.getCountriesByAlphaCodes(codes, fields);
     }
 
     @Override
-    public ResponseEntity<List<Country>> getCountriesByCallingCode(String callingcode, String fields) {
+    public ResponseEntity<List<CountryResponse>> getCountriesByCallingCode(String callingcode, String fields) {
         return CountryApi.super.getCountriesByCallingCode(callingcode, fields);
     }
 
     @Override
-    public ResponseEntity<List<Country>> getCountriesByCapital(String capital, String fields) {
+    public ResponseEntity<List<CountryResponse>> getCountriesByCapital(String capital, String fields) {
         return CountryApi.super.getCountriesByCapital(capital, fields);
     }
 
     @Override
-    public ResponseEntity<List<Country>> getCountriesByCurrency(String currency, String fields) {
+    public ResponseEntity<List<CountryResponse>> getCountriesByCurrency(String currency, String fields) {
         return CountryApi.super.getCountriesByCurrency(currency, fields);
     }
 
     @Override
-    public ResponseEntity<List<Country>> getCountriesByLanguage(String language, String fields) {
+    public ResponseEntity<List<CountryResponse>> getCountriesByLanguage(String language, String fields) {
         return CountryApi.super.getCountriesByLanguage(language, fields);
     }
 
     @Override
-    public ResponseEntity<List<Country>> getCountriesByName(String name, String fields) {
+    public ResponseEntity<List<CountryResponse>> getCountriesByName(String name, String fields) {
         return CountryApi.super.getCountriesByName(name, fields);
     }
 
     @Override
-    public ResponseEntity<List<Country>> getCountriesByRegion(String region, String fields) {
+    public ResponseEntity<List<CountryResponse>> getCountriesByRegion(String region, String fields) {
         return CountryApi.super.getCountriesByRegion(region, fields);
     }
 
     @Override
-    public ResponseEntity<List<Country>> getCountriesByRegionalBloc(String regionalbloc, String fields) {
+    public ResponseEntity<List<CountryResponse>> getCountriesByRegionalBloc(String regionalbloc, String fields) {
         return CountryApi.super.getCountriesByRegionalBloc(regionalbloc, fields);
     }
 
     @Override
-    public ResponseEntity<Country> getCountryByAlphaCode(String code, String fields) {
+    public ResponseEntity<CountryResponse> getCountryByAlphaCode(String code, String fields) {
         return CountryApi.super.getCountryByAlphaCode(code, fields);
     }
 }
