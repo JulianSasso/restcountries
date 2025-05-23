d7e987c9d881d3119fbff4ca95d0cc30cf914950
Julian Sasso <jsasso@itba.edu.ar>
2025-05-23 15:04:35 -0300

    Remove unnecesary CountryJsonMapperImpl

--- src/main/java/eu/fayder/restcountries/infrastructure/persistence/json/CountryJsonMapper.java
+++ src/main/java/eu/fayder/restcountries/infrastructure/persistence/json/CountryJsonMapper.java
@@ -1,10 +1,27 @@
 package eu.fayder.restcountries.infrastructure.persistence.json;
 
 import eu.fayder.restcountries.domain.country.Country;
+import eu.fayder.restcountries.domain.country.CountryCodes;
+import eu.fayder.restcountries.domain.country.Demographics;
+import eu.fayder.restcountries.domain.country.Geography;
+import org.mapstruct.Mapper;
 
 import java.util.List;
 
+@Mapper(componentModel = "spring")
 public interface CountryJsonMapper {
-    Country toDomain(CountryJson json);
+    default Country toDomain(CountryJson json){
+        return new Country(
+                json.getName(),
+            new CountryCodes(json.getTopLevelDomain(), json.getAlpha2Code(), json.getAlpha3Code(), json.getNumericCode(), json.getCioc(), json.getCallingCodes()),
+            new Geography(json.getCapital(), json.getAltSpellings(), json.getRegion(), json.getSubregion(), json.getLatlng(), json.getTimezones(), json.getBorders()),
+            new Demographics(json.getPopulation(), json.getDemonym(), json.getArea(), json.getGini(), json.getNativeName()),
+                json.getCurrencies(),
+                json.getLanguages(),
+                json.getTranslations(),
+                json.getFlag(),
+                json.getRegionalBlocs()
+        );
+    }
 
 }
