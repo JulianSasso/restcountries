c8dd5a2e4a161614dfbf74ed578845e93e29e700
Julian Sasso <jsasso@itba.edu.ar>
2025-05-27 20:04:07 -0300

    Remove unused method in JsonTestUtils

--- src/test/java/eu/fayder/restcountries/testUtils/JsonTestUtils.java
+++ src/test/java/eu/fayder/restcountries/testUtils/JsonTestUtils.java
@@ -1,24 +1,10 @@
 package eu.fayder.restcountries.testUtils;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class JsonTestUtils {
-    private static final ObjectMapper mapper = new ObjectMapper();
-
-    public static boolean jsonEquals(String actualJson, String expectedJson) {
-        try {
-            JsonNode actual = mapper.readTree(actualJson);
-            JsonNode expected = mapper.readTree(expectedJson);
-            return actual.equals(expected);
-        } catch (Exception e) {
-            throw new RuntimeException("Error comparing JSON", e);
-        }
-    }
 
     public static String loadFileContent(String resourcePath) {
         try {
