89ec1911bb4221248e7ab1d4923697cefbd2d006
Julian Sasso <jsasso@itba.edu.ar>
2025-05-27 15:18:49 -0300

    Simplify CountryJsonMapper to be generated by mapstruct

--- src/main/java/eu/fayder/restcountries/infrastructure/persistence/file/json/CountryJsonMapper.java
+++ src/main/java/eu/fayder/restcountries/infrastructure/persistence/file/json/CountryJsonMapper.java
@@ -7,24 +7,44 @@ import eu.fayder.restcountries.infrastructure.persistence.file.json.country.Lang
 import eu.fayder.restcountries.infrastructure.persistence.file.json.country.RegionalBlocJson;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface CountryJsonMapper {
-    default Country toDomain(CountryJson json){
-        return Country.builder()
-                .name(json.getName())
-                .codes(countryCodesFromCountryJson(json))
-                .geography(geographyFromCountryJson(json))
-                .demographics(demographicsFromCountryJson(json))
-                .currencies(json.getCurrencies().stream().map(this::toDomain).toList())
-                .languages(json.getLanguages().stream().map(this::toDomain).toList())
-                .translations(json.getTranslations())
-                .flag(json.getFlag())
-                .regionalBlocs(json.getRegionalBlocs().stream().map(this::toDomain).toList())
+
+    @Mapping(source = "latlng", target = "geography.coordinates", qualifiedByName = "toCoordinates")
+    @Mapping(source = "topLevelDomain", target = "codes.topLevelDomain")
+    @Mapping(source = "alpha2Code", target = "codes.isoAlpha2Code")
+    @Mapping(source = "alpha3Code", target = "codes.isoAlpha3Code")
+    @Mapping(source = "numericCode", target = "codes.isoNumericCode")
+    @Mapping(source = "cioc", target = "codes.iocCode")
+    @Mapping(source = "callingCodes", target = "codes.callingCodes")
+    @Mapping(source = "capital", target = "geography.capital")
+    @Mapping(source = "altSpellings", target = "geography.alternativeSpellings")
+    @Mapping(source = "region", target = "geography.region")
+    @Mapping(source = "subregion", target = "geography.subregion")
+    @Mapping(source = "timezones", target = "geography.timezones")
+    @Mapping(source = "borders", target = "geography.borders")
+    @Mapping(source = "population", target = "demographics.population")
+    @Mapping(source = "demonym", target = "demographics.demonym")
+    @Mapping(source = "area", target = "demographics.area")
+    @Mapping(source = "gini", target = "demographics.giniCoefficient")
+    @Mapping(source = "nativeName", target = "demographics.nativeName")
+    Country toDomain(CountryJson json);
+
+    @Named("toCoordinates")
+    default Coordinates toCoordinates(List<Double> latlng) {
+        if (latlng == null || latlng.size() != 2) return null;
+        return Coordinates.builder()
+                .latitude(latlng.get(0))
+                .longitude(latlng.get(1))
                 .build();
     }
+
+    List<Language> toLanguages(List<LanguageJson> json);
+    List<RegionalBloc> toRegionalBlocs(List<RegionalBlocJson> json);
 
     @Mapping(source = "iso639_1", target = "isoTwoLetterCode")
     @Mapping(source = "iso639_2", target = "isoThreeLetterCode")
@@ -34,47 +54,4 @@ public interface CountryJsonMapper {
 
     RegionalBloc toDomain(RegionalBlocJson json);
 
-    default Demographics demographicsFromCountryJson(CountryJson country) {
-        return Demographics.builder()
-                .population(country.getPopulation())
-                .demonym(country.getDemonym())
-                .area(country.getArea())
-                .giniCoefficient(country.getGini())
-                .nativeName(country.getNativeName())
-                .build();
-    }
-
-    default CountryCodes countryCodesFromCountryJson(CountryJson country) {
-        return CountryCodes.builder()
-                .topLevelDomain(country.getTopLevelDomain())
-                .isoAlpha2Code(country.getAlpha2Code())
-                .isoAlpha3Code(country.getAlpha3Code())
-                .isoNumericCode(country.getNumericCode())
-                .iocCode(country.getCioc())
-                .callingCodes(country.getCallingCodes())
-                .build();
-    }
-
-    default Geography geographyFromCountryJson(CountryJson country){
-        return Geography.builder()
-                .capital(country.getCapital())
-                .alternativeSpellings(country.getAltSpellings())
-                .region(country.getRegion())
-                .subregion(country.getSubregion())
-                .coordinates(coordinatesFromLatlng(country.getLatlng()))
-                .timezones(country.getTimezones())
-                .borders(country.getBorders())
-                .build();
-    }
-
-    default Coordinates coordinatesFromLatlng(List<Double> latlng){
-        if(latlng == null || latlng.size() != 2) {
-            return null;
-        }
-
-        return  Coordinates.builder()
-                    .latitude(latlng.get(0))
-                    .longitude(latlng.get(1))
-                    .build();
-    }
 }
